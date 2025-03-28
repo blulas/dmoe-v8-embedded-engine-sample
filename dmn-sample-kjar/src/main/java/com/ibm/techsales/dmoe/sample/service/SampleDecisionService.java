3ed4dbece4ad625756479b9224dc77c9f6cf2909
Barry Lulas <barry.lulas@ibm.com>
2025-03-28 13:14:38 -0400

    updated version to use BAMOE version

--- dmn-sample-kjar/src/main/java/com/ibm/techsales/dmoe/sample/service/SampleDecisionService.java
+++ dmn-sample-kjar/src/main/java/com/ibm/techsales/dmoe/sample/service/SampleDecisionService.java
@@ -4,11 +4,11 @@ import com.ibm.techsales.dmoe.engine.api.DecisionEngineAdaptor;
 
 public class SampleDecisionService extends DecisionEngineAdaptor {
 
-    private static final String DMN_NAMESPACE         = "NAMESPACE";
-    private static final String DMN_MODEL_NAME        = "MODEL_NAME";
-    private static final String KJAR_GROUP_ID         = "com.ibm.techsales.dmoe.samples";
+    private static final String DMN_NAMESPACE         = "_1C792953-80DB-4B32-99EB-25FBE32BAF9E";
+    private static final String DMN_MODEL_NAME        = "_1C792953-80DB-4B32-99EB-25FBE32BAF9E";
+    private static final String KJAR_GROUP_ID         = "com.ibm.techsales.dmoe.v8.samples";
     private static final String KJAR_ARTIFACT_ID      = "dmoe-v8-embedded-dmn-sample-kjar";
-    private static final String KJAR_VERSION          = "1.0.0-SNAPSHOT";
+    private static final String KJAR_VERSION          = "8.0.6";
 
     public SampleDecisionService() {
         super(DMN_NAMESPACE, DMN_MODEL_NAME, KJAR_GROUP_ID, KJAR_ARTIFACT_ID, KJAR_VERSION);
--- drl-sample-kjar/src/main/java/com/ibm/techsales/dmoe/sample/service/SampleRuleService.java
+++ drl-sample-kjar/src/main/java/com/ibm/techsales/dmoe/sample/service/SampleRuleService.java
@@ -6,9 +6,9 @@ public class SampleRuleService extends RuleEngineAdaptor {
 
     private static final String KIE_SESSION_NAME      = "LoanApplication";
     private static final int    KIE_SESSION_POOL_SIZE = 100;
-    private static final String KJAR_GROUP_ID         = "com.ibm.techsales.dmoe.samples";
+    private static final String KJAR_GROUP_ID         = "com.ibm.techsales.dmoe.v8.samples";
     private static final String KJAR_ARTIFACT_ID      = "dmoe-v8-embedded-drl-sample-kjar";
-    private static final String KJAR_VERSION          = "1.0.0-SNAPSHOT";
+    private static final String KJAR_VERSION          = "8.0.6";
 
     public SampleRuleService() {
         super(KIE_SESSION_NAME, KIE_SESSION_POOL_SIZE, KJAR_GROUP_ID, KJAR_ARTIFACT_ID, KJAR_VERSION);
